afec7363031c319b36bb37476da7588a2d971515
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-11-13 15:17:37 +0100

    dedicated mars repo

--- vertigo-samples/sample-component/src/main/java/io/vertigo/samples/components/a_basics/Calculator2.java
+++
@@ -1,8 +0,0 @@
-package io.vertigo.samples.components.a_basics;
-
-import io.vertigo.core.component.Component;
-
-public interface Calculator2 extends Component {
-
-	int sum(final int... values);
-}
