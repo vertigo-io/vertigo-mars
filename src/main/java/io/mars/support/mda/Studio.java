d8844565123e6bdc6237a13be5a115a1a7521adb
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-04-23 22:47:51 +0200

    update studio config

--- src/main/java/io/mars/support/mda/Studio.java
+++ src/main/java/io/mars/support/mda/Studio.java
@@ -2,14 +2,15 @@ package io.mars.support.mda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 import io.vertigo.commons.CommonsFeatures;
 import io.vertigo.core.node.AutoCloseableApp;
 import io.vertigo.core.node.config.NodeConfig;
-import io.vertigo.core.param.Param;
 import io.vertigo.core.plugins.resource.classpath.ClassPathResourceResolverPlugin;
 import io.vertigo.datastore.DataStoreFeatures;
 import io.vertigo.studio.StudioFeatures;
+import io.vertigo.studio.mda.MdaConfig;
 import io.vertigo.studio.mda.MdaManager;
 import io.vertigo.studio.metamodel.MetamodelResource;
 import io.vertigo.studio.metamodel.StudioMetamodelManager;
@@ -34,18 +35,8 @@ public class Studio {
 				.addModule(new StudioFeatures()
 						.withMetamodel()
 						.withVertigoMetamodel()
-						.withMda(
-								Param.of("projectPackageName", "io.mars"))
-						.withJavaDomainGenerator(
-								Param.of("generateDtResources", "false"))
-						.withTaskGenerator()
-						.withSearchGenerator()
-						//.withFileGenerator()
-						.withSqlDomainGenerator(
-								Param.of("targetSubDir", "javagen/sqlgen"),
-								Param.of("baseCible", "H2"),
-								Param.of("generateDrop", "true"),
-								Param.of("generateMasterData", "true"))
+						.withMda()
+						.withVertigoMda()
 						.build())
 				.build();
 
@@ -56,14 +47,27 @@ public class Studio {
 			final StudioMetamodelManager studioMetamodelManager = studioApp.getComponentSpace().resolve(StudioMetamodelManager.class);
 			final MdaManager mdaManager = studioApp.getComponentSpace().resolve(MdaManager.class);
 			//-----
-			mdaManager.clean();
+
+			final Properties mdaProperties = new Properties();
+			mdaProperties.put("vertigo.domain.java", "true");
+			mdaProperties.put("vertigo.domain.java.generateDtResources", "false");
+			mdaProperties.put("vertigo.domain.sql", "true");
+			mdaProperties.put("vertigo.domain.sql.targetSubDir", "javagen/sqlgen");
+			mdaProperties.put("vertigo.domain.sql.baseCible", "H2");
+			mdaProperties.put("vertigo.domain.sql.generateDrop", "true");
+			mdaProperties.put("vertigo.domain.sql.generateMasterData", "true");
+			mdaProperties.put("vertigo.task", "true");
+			mdaProperties.put("vertigo.search", "true");
+			final MdaConfig mdaConfig = MdaConfig.of("io.mars", mdaProperties);
+
+			mdaManager.clean(mdaConfig);
 			final List<MetamodelResource> resources = Arrays.asList(
 					new MetamodelResource("kpr", "io/mars/model.kpr"),
 					new MetamodelResource("kpr", "io/mars/tasks.kpr"),
 					//new MetamodelResource("kpr", "io/mars/support/support_file.kpr"),
 					new MetamodelResource("kpr", "io/mars/search.kpr"),
 					new MetamodelResource("staticMasterData", "io/mars/support/masterDataValues.json"));
-			mdaManager.generate(studioMetamodelManager.parseResources(resources)).displayResultMessage(System.out);
+			mdaManager.generate(studioMetamodelManager.parseResources(resources), mdaConfig).displayResultMessage(System.out);
 		}
 	}
 
