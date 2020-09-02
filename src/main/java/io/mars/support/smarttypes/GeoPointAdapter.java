5e760af702a45d0ec7a02ad131967afb1ab51f29
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2020-09-02 19:49:08 +0200

    more robust adapter

--- src/main/java/io/mars/support/smarttypes/GeoPointAdapter.java
+++ src/main/java/io/mars/support/smarttypes/GeoPointAdapter.java
@@ -16,8 +16,11 @@ public class GeoPointAdapter implements BasicTypeAdapter<GeoPoint, String> {
 	public GeoPoint toJava(final String geoPointAsJson, final Class<GeoPoint> type) {
 		final JsonElement geoPointAsJsonElement = JsonParser.parseString(geoPointAsJson);
 		if (geoPointAsJsonElement.isJsonObject()) {
-			// GeoPoint object with lat and lon property ex: { "lat": 48.751300, "lon": 2.186570 }
-			return GSON.fromJson(geoPointAsJsonElement, GeoPoint.class);
+			if (geoPointAsJsonElement.getAsJsonObject().has("lat") && geoPointAsJsonElement.getAsJsonObject().has("lon")) {
+				// GeoPoint object with lat and lon property ex: { "lat": 48.751300, "lon": 2.186570 }
+				return GSON.fromJson(geoPointAsJsonElement, GeoPoint.class);
+			}
+			return null;
 		} else if (geoPointAsJsonElement.isJsonPrimitive()) {
 			// a simple string that consist of lat,lon ex : "48.751300,2.186570"
 			final String[] geoPointParts = geoPointAsJson.substring(1, geoPointAsJson.length() - 1).split(",");
