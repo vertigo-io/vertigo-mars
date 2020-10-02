ac563313302b8f468de14d806f71a44941af2afa
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2020-10-02 16:26:31 +0200

    removed some inline

--- src/main/webapp/static/js/mars.js
+++ src/main/webapp/static/js/mars.js
@@ -1,9 +1,21 @@
-VUiExtensions.methods.notificationTypes = function() {
-	return {
-		'MARS-LOGIN' : 'mdi-account-check',
-		'MARS-LOGIN-ATTEMP' : 'mdi-account-alert',
-		'MARS-TICKET-LEDGER' : 'mdi-mdi-issue mdi-ethereum',
-		'MARS-MISSION-LEDGER' : 'mdi-mdi-school mdi-ethereum',
-		'MARS-BASE' : 'fas fa-dice-d20'
+VUiExtensions = {
+	methods : {
+		notificationTypes : function() {
+			return {
+				'MARS-LOGIN' : 'mdi-account-check',
+				'MARS-LOGIN-ATTEMP' : 'mdi-account-alert',
+				'MARS-TICKET-LEDGER' : 'mdi-mdi-issue mdi-ethereum',
+				'MARS-MISSION-LEDGER' : 'mdi-mdi-school mdi-ethereum',
+				'MARS-BASE' : 'fas fa-dice-d20'
+			}
+		}
 	}
+}
+
+quasarConfig = {
+    loadingBar: { 'skip-hijack' : true } // disable quasar's ajaxbar
+}
+
+if(typeof VueShortkey !== 'undefined') {
+	Vue.use(VueShortkey);
 }
