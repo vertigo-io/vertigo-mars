7d48fe626b17099c5abe37eda844d3e3c287b049
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2021-02-17 14:30:10 +0100

    fix geopoint adapter

--- src/main/java/io/mars/support/smarttypes/GeoPointAdapter.java
+++ src/main/java/io/mars/support/smarttypes/GeoPointAdapter.java
@@ -3,6 +3,7 @@ package io.mars.support.smarttypes;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 import io.vertigo.core.lang.BasicType;
 import io.vertigo.core.lang.BasicTypeAdapter;
@@ -14,16 +15,20 @@ public class GeoPointAdapter implements BasicTypeAdapter<GeoPoint, String> {
 
 	@Override
 	public GeoPoint toJava(final String geoPointAsJson, final Class<GeoPoint> type) {
-		final JsonElement geoPointAsJsonElement = JsonParser.parseString(geoPointAsJson);
-		if (geoPointAsJsonElement == null || geoPointAsJsonElement.isJsonNull()) {
-			return null;
-		} else if (geoPointAsJsonElement.isJsonObject() &&
-				geoPointAsJsonElement.getAsJsonObject().has("lat") && geoPointAsJsonElement.getAsJsonObject().has("lon")) {
-			// GeoPoint object with lat and lon property ex: { "lat": 48.751300, "lon": 2.186570 }
-			return GSON.fromJson(geoPointAsJsonElement, GeoPoint.class);
-		} else if (geoPointAsJsonElement.isJsonPrimitive()) {
+		try {
+			final JsonElement geoPointAsJsonElement = JsonParser.parseString(geoPointAsJson);
+
+			if (geoPointAsJsonElement == null || geoPointAsJsonElement.isJsonNull()) {
+				return null;
+			} else if (geoPointAsJsonElement.isJsonObject() &&
+					geoPointAsJsonElement.getAsJsonObject().has("lat") && geoPointAsJsonElement.getAsJsonObject().has("lon")) {
+				// GeoPoint object with lat and lon property ex: { "lat": 48.751300, "lon": 2.186570 }
+				return GSON.fromJson(geoPointAsJsonElement, GeoPoint.class);
+			}
+		} catch (final JsonSyntaxException e) {
+			// It should be a plain string
 			// a simple string that consist of lat,lon ex : "48.751300,2.186570"
-			final String[] geoPointParts = geoPointAsJson.substring(1, geoPointAsJson.length() - 1).split(",");
+			final String[] geoPointParts = geoPointAsJson.split(",");
 			final double lat = Double.parseDouble(geoPointParts[0]);
 			final double lon = Double.parseDouble(geoPointParts[1]);
 			return new GeoPoint(lat, lon);
