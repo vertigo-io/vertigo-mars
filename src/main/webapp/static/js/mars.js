97f8d53b3bcfce2f7d4aa73dc68c014eeacc258b
Piedeloup Nicolas <npi2loup@gmail.com>
2022-05-30 11:44:41 +0200

    [Maintenance] Auto height on workOrderDetailModal

--- src/main/webapp/static/js/mars.js
+++ src/main/webapp/static/js/mars.js
@@ -8,7 +8,24 @@ VUiExtensions = {
 				'MARS-MISSION-LEDGER' : 'mdi-mdi-school mdi-ethereum',
 				'MARS-BASE' : 'fas fa-dice-d20'
 			}
-		}
+		},        
+        getDocHeight : function(doc) {
+            doc = doc || document;
+            // stackoverflow.com/questions/...
+            var body = doc.body, html = doc.documentElement;
+            var height = Math.max( body.scrollHeight, body.offsetHeight, 
+            html.clientHeight, html.scrollHeight, html.offsetHeight );
+            return height;
+        },
+        setIframeHeight : function(ifrm) {
+            var doc = ifrm.contentDocument? ifrm.contentDocument: 
+            ifrm.contentWindow.document;
+            ifrm.style.visibility = 'hidden';
+            ifrm.style.height = "100px"; // reset to minimal height ...
+            // IE opt. for bing/msn needs a bit added or scrollbar appears
+            ifrm.style.height = getDocHeight( doc ) + 4 + "px";
+            ifrm.style.visibility = 'visible';
+        }
 	}
 }
 
