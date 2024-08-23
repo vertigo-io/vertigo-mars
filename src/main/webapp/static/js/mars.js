8a3012d2051dfd69db47da0e26048a7bf8c94b7f
Steven KERDUDOU <steven.kerdudou@kleegroup.com>
2024-08-23 20:20:53 +0200

    Use vu:modal new prop 'autoHeight'

--- src/main/webapp/static/js/mars.js
+++ src/main/webapp/static/js/mars.js
@@ -10,24 +10,6 @@ VUiExtensions = {
 				'MARS-BASE' : 'fas fa-dice-d20'
 			}
 		},        
-        getDocHeight : function(doc) {
-            doc = doc || document;
-            // stackoverflow.com/questions/...
-            var body = doc.body, html = doc.documentElement;
-            var height = Math.max( body.scrollHeight, body.offsetHeight, 
-            html.clientHeight, html.scrollHeight, html.offsetHeight );
-            return height;
-        },
-        setIframeHeight : function(ifrm) {
-			// TODO: review this, onload is not triggered (at least in local)
-            var doc = ifrm.contentDocument? ifrm.contentDocument: 
-            ifrm.contentWindow.document;
-            ifrm.style.visibility = 'hidden';
-            ifrm.style.height = "100px"; // reset to minimal height ...
-            // IE opt. for bing/msn needs a bit added or scrollbar appears
-            ifrm.style.height = getDocHeight( doc ) + 4 + "px";
-            ifrm.style.visibility = 'visible';
-        }
 	}
 }
 
