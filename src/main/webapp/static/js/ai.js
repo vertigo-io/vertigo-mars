53ec5b7d2420f53d374f5aa18e44137fa42cf7c8
Steven KERDUDOU <steven.kerdudou@kleegroup.com>
2025-01-21 18:05:26 +0100

    [Llm] Transport page

--- src/main/webapp/static/js/ai.js
+++ src/main/webapp/static/js/ai.js
@@ -71,9 +71,90 @@ window.addEventListener('vui-after-page-mounted', function(event) {
 						}
 					}
 		        });
+
+
+	function getCardByUri(uri) {
+		return VUiPage.vueData.aiFileResponses.find(card => card.fileUri === uri);
+	}
+	
+	VUiPage.vueData.receivingCalls = [];
+	
 });
 
-
-function getCardByUri(uri) {
-	return VUiPage.vueData.aiFileResponses.find(card => card.fileUri === uri);
+VUiExtensions.methods.analyzeTransportText = function(text) {
+	const newCard = { loading: true, text: text };
+	const index = VUiPage.vueData.aiResponses.push(newCard) - 1;
+						
+	VUiPage.$http.post('_analyze', VUiPage.objectToFormData({ CTX: VUiPage.vueData.CTX, text: text }))
+		.then(response => {
+			VUiPage.vueData.aiResponses[index] = {...response.data, ...VUiPage.vueData.aiResponses[index]};
+			VUiPage.vueData.aiResponses[index].loading = false;
+		})
+		.catch(error => {
+			const card = VUiPage.vueData.aiResponses[index];
+			card.loading = false;
+			card.error = error;
+		});
 }
+
+
+const calls = [
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : mon code est le liv 42 12 et je viens de crever sur l'A86 extérieure près de la sortie 30, je suis sur le bas côté en warning, vous pouvez m'envoyer un dépanneur ?
+Agent : c'est noté, nous vous envoyons un dépanneur de suite.
+Appelant : merci`,
+
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : Bonjour, le panneaux de vitesse est taggé à créteil sur la D86 entre le croisement de la d201 et la station BP.
+Agent : C'est noté merci, quel est vôtre matricule ?
+Appelant : vl 221`,
+
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : Bonjour, je suis le conducteur p l deux cent quarente deux et j'ai un problème avec mon clignotant droite
+Agent : Merci, j'en informe le centre de maintenance`,
+
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : Oui, bon comme d'hab c'est totalement bouché pour accéder à la 118, je vais avoir du retard pour la livraison
+Agent : Merci du signalement, pouvez vous me dire où vous êtes et votre matricule s'il vous plait ?
+Appelant : Oui, VL 512, je suis à boulogne
+Agent : Merci, c'est noté !`,
+
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : oui, c'est PL 52 13, le pont Général-Audibert est fermé, je prend la déviation par le pont de la Haudaudine.`,
+
+`Agent : Bonjour, service technique à votre écoute, pouvez vous me donner votre numéro d'identification et une description de votre problème ?
+Appelant : Bonjour, matricule pl 12 12, c'est pour vous signaler un vehicule arrêté sur le bas côté, périphérique sortie porte de la chapelle.
+Agent : Merci, c'est noté !`,
+];
+
+/*
+Bonjour, mon matricule est le VL_42 et je viens de tomber en panne au niveau du 18 avenue de l'opéra. Pouvez-vous envoyer une dépanneuse ?'
+*/
+
+VUiExtensions.methods.connectToCallCenter = function() {
+	for (const call of calls) {
+		// simulate calls by appening with random delay
+		setTimeout(() => appendCall(call), Math.random() * 20000);
+	}
+}
+
+function appendCall(text) {
+	VUiPage.vueData.clicked=false;
+	var words = text.split(' ');
+	let receivingText  = { text: ''};
+	VUiPage.vueData.receivingCalls.push(receivingText);
+	setTimeout(() => addWord(receivingText, words, 0), Math.random() * 200 + 150);
+}
+
+function addWord(obj, texts, index) {
+	VUiPage.vueData.receivingCalls[VUiPage.vueData.receivingCalls.indexOf(obj)].text = obj.text + ' ' + texts[index];
+	if (index < texts.length - 1) {
+		setTimeout(() => addWord(obj, texts, index + 1), Math.random() * 200 + 150);
+	} else {
+		setTimeout(() => {
+			VUiExtensions.methods.analyzeTransportText (obj.text.substring(obj.text.indexOf("\n") + 1));
+			VUiPage.vueData.receivingCalls.splice(VUiPage.vueData.receivingCalls.indexOf(obj), 1);
+			}, 2000);
+	}
+}
+
